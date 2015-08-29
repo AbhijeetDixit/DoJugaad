75ccde718c1e93fe6df00105af97a2ed8768431d
Abhijeet Dixit <abhijeetdixit27@gmail.com>
2015-08-29 20:58:33 +0530

    Making browse products page

--- public/js/browse.js
+++ public/js/browse.js
@@ -5,7 +5,13 @@ var app = angular.module('myApp',['ngMaterial']);
 
 
 app.controller('browsectrl',function($scope,$location,$http){
+    console.log('In browse controller');
+    $scope.catags = ["Technology","Science","Daily Life","General","Other"];
+    $scope.path = "All Catagories";
 
+    $scope.expand = function () {
+        alert('Expand');
+    }
 
 });
 
